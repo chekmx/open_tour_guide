1ea2e3006fc1be69de094814d1ccc4209f55198f
chekmx <stephen.john.richards@gmail.com>
2018-02-04 10:12:05 +0100

    Switch to using google maps.

--- src/components/SearchTours/SearchTours.component.js
+++ src/components/SearchTours/SearchTours.component.js
@@ -1,36 +1,15 @@
-import Vue2Leaflet from 'vue2-leaflet'
-import L from 'leaflet'
-
-import icon from 'leaflet/dist/images/marker-icon.png'
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
-
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow
-})
-
-L.Marker.prototype.options.icon = DefaultIcon
-
 export default {
   name: 'search-tours',
   components: {
-    'v-map': Vue2Leaflet.Map,
-    'v-tilelayer': Vue2Leaflet.TileLayer,
-    'v-marker': Vue2Leaflet.Marker
   },
   props: [],
   data () {
     return {
-      center: [47.327060, 8.801356],
-      position: [47.413220, -1.219482],
-      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
+      zoom: 12,
+      center: { lat: 47.327060, lng: 8.801356 },
       markers: [
-        { id: 'm1', position: {lat: 51.505, lng: -0.09}, tooltip: 'tooltip for marker1', draggable: true, visible: true },
-        { id: 'm2', position: {lat: 51.8905, lng: -0.09}, tooltip: 'tooltip for marker2', draggable: true, visible: false },
-        { id: 'm3', position: {lat: 51.005, lng: -0.09}, tooltip: 'tooltip for marker3', draggable: true, visible: true },
-        { id: 'm4', position: {lat: 50.7605, lng: -0.09}, tooltip: 'tooltip for marker4', draggable: true, visible: false }
+        { position: { lat: 47.327060, lng: 8.801356 }, title: 'Test' },
+        { position: { lat: 47.327060, lng: 8.901346 }, title: 'Test2' }
       ]
     }
   },
--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
-import L from 'leaflet'
-import Vue2Leaflet from 'vue2-leaflet'
-import icon from 'leaflet/dist/images/marker-icon.png'
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
-
+import * as VueGoogleMaps from 'vue2-google-maps'
 import App from './App'
 import router from './router'
 
 Vue.use(Vuetify)
-Vue.component('v-map', Vue2Leaflet.Map)
-Vue.component('v-tilelayer', Vue2Leaflet.TileLayer)
-Vue.component('v-marker', Vue2Leaflet.Marker)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAwCwjczJwZamSM-XapQcqNVJ0JUQSK-OI',
+    libraries: 'places'
+  }
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -24,19 +23,3 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
-
-// eslint-disable-next-line  
-delete L.Icon.Default.prototype._getIconUrl  
-// eslint-disable-next-line  
-L.Icon.Default.mergeOptions({  
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
-
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow
-})
-
-L.Marker.prototype.options.icon = DefaultIcon
